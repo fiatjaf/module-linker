b96b407afabcd65ceaf1f266fa19aed0bd2187f0
fiatjaf@spooner <fiatjaf@gmail.com>
2016-03-13 12:22:18 -0400

    js imports with files ending in ".js"

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 import $ from 'jquery'
 import startswith from 'lodash.startswith'
+import endswith from 'lodash.endswith'
 
 const stdlib = {assert: 1, buffer: 1, addons: 1, child_process: 1, cluster: 1, console: 1, crypto: 1, debugger: 1, dns: 1, domain: 1, errors: 1, events: 1, fs: 1, globals: 1, http: 1, https: 1, modules: 1, net: 1, os: 1, path: 1, process: 1, punycode: 1, querystring: 1, readline: 1, repl: 1, stream: 1, string_decoder: 1, timers: 1, tls: 1, tty: 1, dgram: 1, url: 1, util: 1, v8: 1, vm: 1, zlib: 1}
 
@@ -25,7 +26,11 @@ export function process () {
 
     var url
     if (startswith(moduleName, '.')) {
-      url = moduleName + '.js'
+      if (endswith(moduleName, '.js') || endswith(moduleName, '.coffee') || endswith(moduleName, '.ts')) {
+        url = moduleName
+      } else {
+        url = moduleName + '.js'
+      }
     } else if (moduleName in stdlib) {
       url = 'https://nodejs.org/api/' + moduleName + '.html'
     } else {
