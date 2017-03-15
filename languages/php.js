854a0ebe4e0280d76732450d79bf344ae04dcb69
fiatjaf <fiatjaf@mises>
2017-03-15 20:37:08 -0300

    implement external module fetch caching for the rest of languages.

--- languages/php.js
+++ languages/php.js
@@ -1,5 +1,6 @@
 const fetch = window.fetch
 
+var waiting = {} // a cache of promises to php external modules
 module.exports.composerurl = composerurl
 function composerurl (moduleName) {
   if (moduleName.split('/').length === 1) {
@@ -8,8 +9,12 @@ function composerurl (moduleName) {
     return Promise.reject()
   }
 
-  return fetch(`https://githublinker.herokuapp.com/q/composer/${moduleName}`)
-    .then(r => r.json())
-    .then(({url}) => url)
-    .catch(() => `https://packagist.org/packages/${moduleName}`)
+  if (!waiting[moduleName]) {
+    waiting[moduleName] = fetch(`https://githublinker.herokuapp.com/q/composer/${moduleName}`)
+      .then(r => r.json())
+      .then(({url}) => url)
+      .catch(() => `https://packagist.org/packages/${moduleName}`)
+  }
+
+  return waiting[moduleName]
 }
--- languages/ruby.js
+++ languages/ruby.js
@@ -43,10 +43,7 @@ module.exports.process = function process () {
         if (moduleName in stdlib) {
           return 'http://ruby-doc.org/stdlib/libdoc/' + moduleName + '/rdoc/index.html'
         } else {
-          return fetch(`https://githublinker.herokuapp.com/q/rubygems/${moduleName}`)
-            .then(r => r.json())
-            .then(({url}) => url)
-            .catch(() => 'https://rubygems.org/gems/' + moduleName)
+          return rubygemsurl(moduleName)
         }
       })
       .then(url => {
@@ -54,6 +51,19 @@ module.exports.process = function process () {
       })
     })(el)
   })
+}
+
+var waiting = {} // a cache of promises to ruby external modules
+module.exports.rubygemsurl = rubygemsurl
+function rubygemsurl (moduleName) {
+  if (!waiting[moduleName]) {
+    waiting[moduleName] = fetch(`https://githublinker.herokuapp.com/q/rubygems/${moduleName}`)
+      .then(r => r.json())
+      .then(({url}) => url)
+      .catch(() => 'https://rubygems.org/gems/' + moduleName)
+  }
+
+  return waiting[moduleName]
 }
 
 const stdlib = {abbrev: 1, base64: 1, benchmark: 1, bigdecimal: 1, cgi: 1, cmath: 1, coverage: 1, csv: 1, date: 1, dbm: 1, debug: 1, delegate: 1, digest: 1, drb: 1, e2mmap: 1, English: 1, erb: 1, etc: 1, expect: 1, extmk: 1, fcntl: 1, fiddle: 1, fileutils: 1, find: 1, forwardable: 1, gdbm: 1, getoptlong: 1, 'io/console': 1, 'io/nonblock': 1, 'io/wait': 1, ipaddr: 1, irb: 1, json: 1, logger: 1, mathn: 1, matrix: 1, mkmf: 1, monitor: 1, mutex_m: 1, 'net/ftp': 1, 'net/http': 1, 'net/imap': 1, 'net/pop': 1, 'net/smtp': 1, 'net/telnet': 1, nkf: 1, objspace: 1, observer: 1, 'open-uri': 1, open3: 1, openssl: 1, optparse: 1, ostruct: 1, pathname: 1, pp: 1, prettyprint: 1, prime: 1, profile: 1, profiler: 1, pstore: 1, psych: 1, pty: 1, racc: 1, 'racc/parser': 1, rake: 1, rdoc: 1, readline: 1, resolv: 1, 'resolv-replace': 1, rexml: 1, rinda: 1, ripper: 1, rss: 1, rubygems: 1, scanf: 1, sdbm: 1, securerandom: 1, set: 1, shell: 1, shellwords: 1, singleton: 1, socket: 1, stringio: 1, strscan: 1, sync: 1, syslog: 1, tempfile: 1, thread: 1, thwait: 1, time: 1, timeout: 1, tk: 1, tmpdir: 1, tracer: 1, tsort: 1, un: 1, unicode_normalize: 1, uri: 1, weakref: 1, webrick: 1, win32ole: 1, xmlrpc: 1, yaml: 1, zlib: 1}
