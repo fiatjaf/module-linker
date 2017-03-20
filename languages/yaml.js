48b5a8f7772215e1cfce0adfed62447b61fde2e6
Ludwig Lachmann <fiatjaf@gmail.com>
2017-03-20 12:11:17 -0300

    pubspec.yaml

--- languages/yaml.js
+++ languages/yaml.js
@@ -1,14 +1,23 @@
 const $ = window.jQuery
 
+const darturl = require('./dart').darturl
+
 const createLink = require('../helpers').createLink
 
 module.exports.process = function process () {
-  if (location.pathname.split('/').slice(-1)[0].match(/stack[\d.-]*\.yaml/)) {
+  if (window.pathdata.last.match(/stack[\d.-]*\.yaml/)) {
     stackyaml()
+  } else {
+    switch (window.pathdata.last) {
+      case 'pubspec.yaml':
+        pubspecyaml()
+        break
+    }
   }
 }
 
 function stackyaml () {
+  // haskell
   var depsOpen = false
 
   $('.blob-code-inner').each((_, elem) => {
@@ -29,6 +38,34 @@ function stackyaml () {
     }
 
     if (!line.match(/^- /)) {
+      depsOpen = false
+    }
+  })
+}
+
+function pubspecyaml () {
+  // dart
+  var depsOpen = false
+
+  $('.blob-code-inner').each((_, elem) => {
+    elem = $(elem)
+    let rawline = elem.text()
+
+    if (rawline.match(/^dependencies:/) || rawline.match(/^dev_dependencies:/)) {
+      depsOpen = true
+      return
+    }
+
+    if (depsOpen && rawline.match(/^ +[\w_]*:/)) {
+      let link = elem.find('.pl-ent').eq(0)
+      let moduleName = link.text().trim()
+
+      darturl(moduleName)
+        .catch(() => url => `pub.dartlang.org/api/packages/${moduleName}`)
+        .then(url => createLink(link.get(0), moduleName, url))
+    }
+
+    if (!rawline.match(/^ /)) {
       depsOpen = false
     }
   })
