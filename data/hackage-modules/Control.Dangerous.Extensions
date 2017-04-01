ff287a3d96297c7f5b87f8ad46052d471d55f0de
fiatjaf <fiatjaf@gmail.com>
2017-04-01 23:22:26 +0000

    update hackage data.

---
+++ data/hackage-modules/Control.Dangerous.Extensions
@@ -0,0 +1,1 @@
+Dangerous
--- data/hackage-update.fish
+++ data/hackage-update.fish
@@ -6,9 +6,9 @@ wget https://hackage.haskell.org/packages/index.tar.gz -O hackage-all.tar.gz
 tar -xvf hackage-all.tar.gz > /dev/null
 rm hackage-all.tar.gz
 
-for pack in (ls h/)
-  set vers (ls -t1 h/$pack/ | grep -v 'version' | tail -n 1)
-  set modules (cat h/$pack/$vers/$pack.cabal | python3 hackage-parse.py)
+for pack in (ls)
+  set vers (ls -t1 $pack/ | grep -v 'version' | tail -n 1)
+  set modules (cat $pack/$vers/$pack.cabal | python3 $here/hackage-parse.py)
   for m in $modules
     echo $pack > "$here/hackage-modules/$m"
   end
