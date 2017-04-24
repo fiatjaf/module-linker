dfad9f5961780281a0f142afb79bc207093460cd
fiatjaf <fiatjaf@gmail.com>
2017-04-24 15:12:54 +0000

    hackage gets too much updates.

--- data/hackage-update.fish
+++ data/hackage-update.fish
@@ -8,6 +8,7 @@ tar -xvf hackage-all.tar.gz > /dev/null
 rm hackage-all.tar.gz
 
 for pack in (ls -r)
+  echo $pack
   set vers (ls -t1 $pack/ | grep -v 'version' | tail -n 1)
   set modules (cat $pack/$vers/$pack.cabal | python3 $here/hackage-parse.py)
   for m in $modules
