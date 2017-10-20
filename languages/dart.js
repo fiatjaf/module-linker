a889ea9bfaa4b885481a8c530fd5c75cf9874b89
fiatjaf <fiatjaf@gmail.com>
2017-10-20 19:03:49 -0200

    also look into local tree for dart package: imports.

--- languages/dart.js
+++ languages/dart.js
@@ -1,11 +1,27 @@
 const $ = window.jQuery
 const startswith = require('lodash.startswith')
+const endswith = require('lodash.endswith')
 const resolve = require('resolve-pathname')
 
 const external = require('../helpers').external
 const createLink = require('../helpers').createLink
 const htmlWithLink = require('../helpers').htmlWithLink
+const treePromise = require('../helpers').treePromise
 const bloburl = require('../helpers').bloburl
+
+function treeProcess (tree) {
+  let paths = tree
+    .filter(path => startswith(path, 'lib/'))
+    .filter(path => endswith(path, '.dart'))
+
+  var filemap = {}
+  for (let i = 0; i < paths.length; i++) {
+    let path = paths[i]
+    filemap[path.split('/').slice(1).join('/')] = true
+  }
+
+  return filemap
+}
 
 module.exports.process = function process () {
   let { current } = window.pathdata
@@ -37,19 +53,30 @@ function processLine (elem, line, currentPath, lineIndex) {
   Promise.resolve()
   .then(() => {
     if (startswith(moduleName, 'package:')) {
-      // is an external package.
+      // may be an external package or a local package submodule
       let [_, importedpath] = moduleName.split(':')
       let [name, ...extra] = importedpath.split('/')
       let extrapath = extra.join('/')
-      return darturl(name)
-        .then(info => {
-          if (extrapath) {
-            let gh = info.url.match(/https?:\/\/github.com\/([^\/]+)\/([^\/]+)/)
-            if (gh) {
-              info.url = `https://github.com/${gh[1]}/${gh[2]}/blob/master/lib/${extrapath}`
-            }
+
+      return treePromise(treeProcess)
+        .then(filemap => {
+          // found it here -- BEWARE: this code doesn't check the name of the package.
+          if (filemap[extrapath]) {
+            let {user, repo, ref, _} = window.pathdata
+            return bloburl(user, repo, ref, 'lib/' + extrapath)
           }
-          return info
+
+          return darturl(name)
+            .then(info => {
+              if (extrapath) {
+                let gh = info.url.match(/https?:\/\/github.com\/([^\/]+)\/([^\/]+)/)
+                if (gh) {
+                  info.url = `https://github.com/${gh[1]}/${gh[2]}` +
+                             `/blob/master/lib/${extrapath}`
+                }
+              }
+              return info
+            })
         })
     } else if (startswith(moduleName, 'dart:')) {
       // is from the stdlib
