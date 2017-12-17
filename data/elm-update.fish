3eaed339b0c69d572ec3931ccc9439a4fa7ef592
fiatjaf <fiatjaf@gmail.com>
2017-12-17 13:11:23 -0200

    elm-modules with descriptions.

--- data/elm-update.fish
+++ data/elm-update.fish
@@ -5,11 +5,13 @@ mkdir /tmp/elm
 cd /tmp/elm
 wget 'http://package.elm-lang.org/all-packages' -O elm-all.json
 for pkg in (cat elm-all.json | jq -r '.[].name')
-    echo $pkg
-    set modules (curl --silent "http://package.elm-lang.org/packages/$pkg/latest/documentation.json" | jq -r '.[].name')
-    for module in $modules
-        echo $pkg > $here/elm-modules/$module
-    end
-    sleep 1
+  set desc (curl -s "http://package.elm-lang.org/packages/$pkg/latest/elm-package.json" | jq -rc '.summary')
+  echo "$pkg: $desc"
+  for moduleName in (curl -s "http://package.elm-lang.org/packages/$pkg/latest/documentation.json" | jq -rc '.[].name')
+    echo "  $moduleName"
+    echo $pkg > $here/elm-modules/$moduleName
+    echo "|" >> $here/elm-modules/$moduleName
+    echo $desc >> $here/elm-modules/$moduleName
+  end
 end
 rm elm-all.json
