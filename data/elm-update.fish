b3eaad7f665d89c76af2b5ee8fe32e8d5cc16fb9
fiatjaf <fiatjaf@gmail.com>
2018-10-17 11:01:55 -0300

    update elm script and elm modules.

--- data/elm-update.fish
+++ data/elm-update.fish
@@ -4,11 +4,18 @@ rm -rf /tmp/elm
 mkdir /tmp/elm
 cd /tmp/elm
 wget 'http://package.elm-lang.org/all-packages' -O elm-all.json
-for pkg in (cat elm-all.json | jq -r '.[].name')
-  set desc (curl -s "http://package.elm-lang.org/packages/$pkg/latest/elm-package.json" | jq -rc '.summary')
-  echo "$pkg: $desc"
-  for moduleName in (curl -s "http://package.elm-lang.org/packages/$pkg/latest/documentation.json" | jq -rc '.[].name')
-    echo "  $moduleName"
+for pkg in (cat elm-all.json | jq -r 'keys | .[]')
+  echo "$pkg"
+  set version (cat elm-all.json | jq -r --arg PKG "$pkg" '.[$PKG][-1]')
+  echo "  version: $version"
+  set desc (curl -s "https://raw.githubusercontent.com/$pkg/master/elm.json" | jq -rc '.summary' 2> /dev/null)
+  if [ -z "$desc" ]
+    set desc (curl -s "https://raw.githubusercontent.com/$pkg/master/elm-package.json" | jq -rc '.summary')
+  end
+  echo "  desc: $desc"
+  echo "  modules:"
+  for moduleName in (curl -s "https://package.elm-lang.org/packages/$pkg/$version/docs.json" | jq -rc '.[].name')
+    echo "    $moduleName"
     echo $pkg > $here/elm-modules/$moduleName
     echo "|" >> $here/elm-modules/$moduleName
     echo $desc >> $here/elm-modules/$moduleName
