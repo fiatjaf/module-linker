565d6a010636e4db73e8ae529dbb97155770bd29
Serhii Khoma <srghma@gmail.com>
2021-08-12 14:51:28 +0300

    fix: set: Tried to change the read-only variable version

--- data/elm-update.fish
+++ data/elm-update.fish
@@ -6,15 +6,15 @@ cd /tmp/elm
 wget 'http://package.elm-lang.org/all-packages' -O elm-all.json
 for pkg in (cat elm-all.json | jq -r 'keys | .[]')
   echo "$pkg"
-  set version (cat elm-all.json | jq -r --arg PKG "$pkg" '.[$PKG][-1]')
-  echo "  version: $version"
+  set vers (cat elm-all.json | jq -r --arg PKG "$pkg" '.[$PKG][-1]')
+  echo "  version: $vers"
   set desc (curl -s "https://raw.githubusercontent.com/$pkg/master/elm.json" | jq -rc '.summary' 2> /dev/null)
   if [ -z "$desc" ]
     set desc (curl -s "https://raw.githubusercontent.com/$pkg/master/elm-package.json" | jq -rc '.summary')
   end
   echo "  desc: $desc"
   echo "  modules:"
-  for moduleName in (curl -s "https://package.elm-lang.org/packages/$pkg/$version/docs.json" | jq -rc '.[].name')
+  for moduleName in (curl -s "https://package.elm-lang.org/packages/$pkg/$vers" | jq -rc '.[].name')
     echo "    $moduleName"
     echo $pkg > $here/elm-modules/$moduleName
     echo "|" >> $here/elm-modules/$moduleName
